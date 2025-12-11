76a2f9f5fe8476677540f53c046bdc6f2232b87e
whkdw <47697987+whkdw@users.noreply.github.com>
2025-12-11 07:34:59 +0000

    update

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ BUILD_STR = {
     -3: "very light", -2: "light", -1: "a little light",
      0: "normal", 1: "a little heavy", 2: "heavy", 3: "very heavy"
 }
-CUTS_STR = {1: "low", 2: "normal", 3: "high"}
+CUTS_STR = [ "low", "normal", "high" ]
 STATS_STR = ["strength", "knockout punch", "speed", "agility", "chin", "conditioning", "random"]
 TRAIN_STR = ["weights+(STR)", "heavy+bag+(KP)", "speed+bag+(SPD)", "jump+rope+(AGL)", "sparring+(CHN)", "road+work+(CND)", "free+practice"]
 DIVISIONS = [
@@ -646,9 +646,11 @@ if __name__ == "__main__":
         raise
 
 
-    print(find_name())
+    #print(find_name())
     print(GYM_PASSWORD, GYM_USERNAME)
 
+    #https://webl.vivi.com/cgi-bin/query.fcgi?competition=eko&command=eko_managerbyid&manager_to_view=77894
+    #to_manager=&77894   your_team=Sunny&    command=eko_transfer
     """for word in write_msg("eko_retired_fighters").split("<H4>Heavyweights")[1].split("Activate</A>"):
         if "regional_champion" not in word and "challenger.gif" not in word and "champion.gif" not in word:
             for team_id in re.findall(r"team_id=([0-9]+)", word):
@@ -656,3 +658,105 @@ if __name__ == "__main__":
                 break
     """
     
+    fighter = """
+    <HTML>
+    <HEAD>
+    <script>
+    document.cookie='timezone='+(new Date()).getTimezoneOffset();
+    </script>
+    <link rel=STYLESHEET type=text/css href=/eko/eko.css>
+    <TITLE>WEBL Query Server (control)</TITLE>
+</HEAD><BODY bgcolor=#eeeeee>
+<center>
+<img src="https://webl.vivi.com/images/webltitle.gif" width="200" height="110">
+</center>
+
+<BR><CENTER><SMALL>
+
+</SMALL></CENTER>
+<HR><BR><B>
+<A name="df" HREF="https://webl.vivi.com/cgi-bin/query.fcgi?+command=eko_career_privatebyid&+competition=eko&+division=Heavy&+region=16097&+team_id=544246">df</A> </B> (0-0-0 0/0)<BR>
+<table border=1>
+<TR><TD>Strength <TD align=right>13<BR>
+<TD>Knockout Punch <TD  align=right>0<BR>
+<TR><TD>Speed <TD align=right>18<BR>
+<TD>Agility <TD align=right>7<BR>
+<TR><TD>Chin <TD align=right>9<BR>
+<TD>Conditioning <TD align=right>12<BR>
+<TR><TD>Cut Resistance <TD align=right>High<BR>
+<TD><a target=glossary onClick=help() href=https://cloudfront-webl.vivi.com/eko/glossary.html#streak>Winning Streak</A><TD align=right>0<TR><TD><script language=javascript>
+    <!--
+    function help() {
+	window.open("", "glossary", "width=550,height=300,resizable=1,scrollbars=1");
+    }
+    //-->
+</script>
+<a href=https://cloudfront-webl.vivi.com/eko/glossary.html#rating target=glossary onClick=help()>Rating</A><TD align=right>0
+<TD><a href=https://cloudfront-webl.vivi.com/eko/glossary.html#status target=glossary onClick=help()>Status</A><TD align=right>0
+<TR><TD><BR><TD><BR>
+<TD>Total Earnings:<TD align=right>  $0 <TR><TD><a href=https://cloudfront-webl.vivi.com/eko/glossary.html#injury target=glossary onClick=help()>Injury Points</A><TD align=right>0
+<TD>AP Loss</A><TD align=right>0
+<TR><TD>Height<TD colspan=3>5 feet 10 inches (177 centimeters)
+<TR><TD>Build<TD colspan=3>normal
+<TR><TD><a href=https://cloudfront-webl.vivi.com/eko/glossary.html#fweight target=glossary onClick=help()>Weight</A> <TD colspan=3>202 pounds (91 kilograms)
+<TR><TD><a href=https://cloudfront-webl.vivi.com/eko/glossary.html#minweight target=glossary onClick=help()>Minimum Weight</A> <TD colspan=3>194 pounds (87 kilograms)
+</table>
+<P>df fights in the <A name="Heavyweight" HREF="https://webl.vivi.com/cgi-bin/query.fcgi?+command=eko_standings&+competition=eko&+division=Heavy&+region=1234567891&team=df">Heavyweight</A> division.  <P>df's next bout is a <B>title fight</B> against the 6 feet 2 inches, 
+<A name="sunny" HREF="https://webl.vivi.com/cgi-bin/query.fcgi?+command=eko_careerbyid&+competition=eko&+division=Heavy&+region=16097&+team_id=1695461&describe=1">Sunny</A>  (0-0-0 0/0)  from the 
+<A HREF="https://webl.vivi.com/cgi-bin/query.fcgi?competition=eko&command=eko_managerbyid&manager_to_view=74575">1234567891</A> gym  for   $0  on <B>Sunday, December 14, 2025.</B>
+<P>df is training <b>random</b>, but you can instruct him to <A  HREF="/cgi-bin/prompt.fcgi?+command=eko_training&+competition=eko&+division=Heavy&+region=16097&+team=df">train for something else</A>.<P>df is currently using the <b>Rely on Hand Speed</b> fight plan. 
+He may <UL>
+<LI><A  HREF="/cgi-bin/prompt.fcgi?+command=eko_select_orders&+competition=eko&+division=Heavy&+region=16097&+team=df">choose a different fight plan</A>, 
+<LI><A  HREF="https://webl.vivi.com/cgi-bin/query.fcgi?+command=query_echo&+competition=eko&+division=Heavy&+filename=fightplan_beginner.html&+region=16097&team=df">create a new fight plan</A>  
+<LI>or <A  HREF="/cgi-bin/prompt.fcgi?+command=query_edit_orders&+competition=eko&+division=Heavy&+region=16097&+strategy_choice=Rely+on+Hand+Speed&strategy_id=555324">edit</A> your <B>Rely on Hand Speed</B> plan.
+
+<LI>Experts can use the <A  HREF="/cgi-bin/prompt.fcgi?+command=query_orders&+competition=eko&+division=Heavy&+region=16097&+team=df">advanced fight plan form</A>.
+
+<LI>Beginners might want to get into the ring with a  <A  HREF="https://webl.vivi.com/cgi-bin/query.fcgi?+command=query_echo&+competition=eko&+division=Heavy&+filename=newbie_form.html&+region=16097&cname=df&fighter_name=df&team_id=544246">sparring partner.</A>
+
+</UL>
+<P>df may also do any of the following:<UL>
+<LI><A  HREF="/cgi-bin/prompt.fcgi?+command=query_press&+competition=eko&+division=Heavy&+region=16097&+team=df&team_id=544246">issue a press release.</A> 
+<LI><A  HREF="/cgi-bin/prompt.fcgi?+command=eko_change_division&+competition=eko&+division=Heavy&+region=16097&+team=df">change weight division.</A>
+<LI><A  HREF="/cgi-bin/prompt.fcgi?+command=eko_rename&+competition=eko&+division=Heavy&+region=16097&+team=df">change his description.</A>
+<LI><A  HREF="/cgi-bin/prompt.fcgi?+command=eko_retire_byid&+competition=eko&+division=Heavy&+region=16097&+team_id=544246">retire.</A></UL>
+
+</BODY></HTML>
+
+"""
+
+    html = fighter
+    data = {}
+
+    data["NAME"] = m.group(1).strip() if (m := re.search(r"<P>(.*?) fights in the", html, re.DOTALL | re.IGNORECASE)) else ""
+
+    data["STRENGTH"] = int(m.group(1)) if ((m := re.search(r"[Dd]>[Ss]trength[^0-9]+(\d+)", html)) and m.group(1).isdigit()) else 0
+    data["KP"] = int(m.group(1)) if ((m := re.search(r"[Dd]>[Kk]nockout[^0-9]+(\d+)", html)) and m.group(1).isdigit()) else 0
+    data["SPEED"] = int(m.group(1)) if ((m := re.search(r"[Dd]>[Ss]peed[^0-9]+(\d+)", html)) and m.group(1).isdigit()) else 0
+    data["AGILITY"] = int(m.group(1)) if ((m := re.search(r"[Dd]>[Aa]gility[^0-9]+(\d+)", html)) and m.group(1).isdigit()) else 0
+    data["CHIN"] = int(m.group(1)) if ((m := re.search(r"[Dd]>[Cc]hin[^0-9]+(\d+)", html)) and m.group(1).isdigit()) else 0
+    data["CONDITIONING"] = int(m.group(1)) if ((m := re.search(r"[Dd]>[Cc]onditioning[^0-9]+(\d+)", html)) and m.group(1).isdigit()) else 0
+
+    data["CUT"] = (CUTS_STR.index(m.group(1).strip().lower()) if ((m := re.search(r"Cut Resistance <[^>]*>([a-zA-Z]+)", html)) and m.group(1)) else 0) + 1
+
+    data["RATING"] = int(m.group(1)) if ((m := re.search(r"Rating[^0-9]+ight>(\d+)", html)) and m.group(1).isdigit()) else 0
+    data["STATUS"] = int(m.group(1)) if ((m := re.search(r"Status[^0-9]+ight>(\d+)", html)) and m.group(1).isdigit()) else 0
+
+
+    # Height: "Height<TD colspan=3>(\d+) feet ?([0-9]{0,2})"
+    m = re.search(r"Height<TD colspan=3>(\d+)\s*feet\s*([0-9]{0,2})", html)
+    if m:
+        data["HEIGHT"] = convert_height(int(m.group(1)), int(m.group(2) or 0))
+    else:
+        data["HEIGHT"] = 0
+
+    # Build
+    m = re.search(r"Build<TD colspan=3>([a-zA-Z ]+)", html)
+    if m:
+        build_str = m.group(1).strip().lower()
+        # reverse mapping: find key in BUILD_STR with matching string
+        found = next((k for k, v in BUILD_STR.items() if v.lower() == build_str), 0)
+        data["BUILD"] = found
+    else:
+        data["BUILD"] = 0
+    print(data)
